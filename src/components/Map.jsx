a0b00d060797b75512f1b297c22762e174fc5351
RanjoMondragon <rvmondragon96@gmail.com>
2023-03-10 08:37:26 -0500

    Add google maps api

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Rating } from '@mui/material';
 
-function Map() {
+const Map = () => {
+  const coordinates = {lat: 0, lng: 0};
   return (
-    <div>Map</div>
+    <div style={{ height: '85vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
